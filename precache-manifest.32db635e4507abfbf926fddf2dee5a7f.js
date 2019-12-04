0dc8a23e3e05c7b45afa87b1b9202963382ee18b
dauiel <928299621@qq.com>
2019-12-05 00:23:24 +0800

    1

--- precache-manifest.32db635e4507abfbf926fddf2dee5a7f.js
+++ precache-manifest.32db635e4507abfbf926fddf2dee5a7f.js
@@ -5,58 +5,58 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "ba4bfc7d45aa95b68a46",
-    "url": "/static/css/2.ac46b9c1.chunk.css"
+    "url": "/douban/static/css/2.ac46b9c1.chunk.css"
   },
   {
     "revision": "f74ae119086d1110c017",
-    "url": "/static/css/main.95c28714.chunk.css"
+    "url": "/douban/static/css/main.95c28714.chunk.css"
   },
   {
     "revision": "ba4bfc7d45aa95b68a46",
-    "url": "/static/js/2.ec1c3564.chunk.js"
+    "url": "/douban/static/js/2.ec1c3564.chunk.js"
   },
   {
     "revision": "f74ae119086d1110c017",
-    "url": "/static/js/main.d054d7b0.chunk.js"
+    "url": "/douban/static/js/main.d054d7b0.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
-    "url": "/static/js/runtime~main.a8a9905a.js"
+    "url": "/douban/static/js/runtime~main.a8a9905a.js"
   },
   {
     "revision": "068ca2b316db98037bebdd1e4f1b9459",
-    "url": "/static/media/fontello.068ca2b3.ttf"
+    "url": "/douban/static/media/fontello.068ca2b3.ttf"
   },
   {
     "revision": "8d4a4e6f7431d0d7fa92b1df20f38161",
-    "url": "/static/media/fontello.8d4a4e6f.woff2"
+    "url": "/douban/static/media/fontello.8d4a4e6f.woff2"
   },
   {
     "revision": "9354499c2824248511adf85fdf8e4c37",
-    "url": "/static/media/fontello.9354499c.svg"
+    "url": "/douban/static/media/fontello.9354499c.svg"
   },
   {
     "revision": "a782baa8633b1359f9686ffad17e0d76",
-    "url": "/static/media/fontello.a782baa8.woff"
+    "url": "/douban/static/media/fontello.a782baa8.woff"
   },
   {
     "revision": "e73a0647198cfe970de0f003be95cc51",
-    "url": "/static/media/fontello.e73a0647.eot"
+    "url": "/douban/static/media/fontello.e73a0647.eot"
   },
   {
     "revision": "112ff81087ac51eba5aee818268ee015",
-    "url": "/static/media/icomoon.112ff810.woff"
+    "url": "/douban/static/media/icomoon.112ff810.woff"
   },
   {
     "revision": "3c2e40124c33fdfeb02c848d0fafec31",
-    "url": "/static/media/icomoon.3c2e4012.ttf"
+    "url": "/douban/static/media/icomoon.3c2e4012.ttf"
   },
   {
     "revision": "82e201ebb22d76391fb4ae03d4edd1f5",
-    "url": "/static/media/icomoon.82e201eb.eot"
+    "url": "/douban/static/media/icomoon.82e201eb.eot"
   },
   {
     "revision": "de24429632a1b3eea7e38ae199a43f37",
-    "url": "/static/media/icomoon.de244296.svg"
+    "url": "/douban/static/media/icomoon.de244296.svg"
   }
 ]);

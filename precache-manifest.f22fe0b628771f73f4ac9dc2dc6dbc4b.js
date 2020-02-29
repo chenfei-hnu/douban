75e5c35b54ecc792b869bd7adf8ed9d446312af7
Ray <928299621@qq.com>
2020-02-29 23:44:39 +0800

    Update precache-manifest.f22fe0b628771f73f4ac9dc2dc6dbc4b.js

--- precache-manifest.f22fe0b628771f73f4ac9dc2dc6dbc4b.js
+++ precache-manifest.f22fe0b628771f73f4ac9dc2dc6dbc4b.js
@@ -1,46 +1,46 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3607d7f8ee2045466dd8f9f07c9c7b70",
+    "revision": "94fcc3b59527f752322c107eeb599869",
     "url": "./index.html"
   },
   {
     "revision": "0c762094c435d895f983",
-    "url": "https://douban-1300076408.cos.ap-guangzhou.myqcloud.com/static/css/2.d50a9c33.chunk.css"
+    "url": "https://introfei.github.io/douban/static/css/2.d50a9c33.chunk.css"
   },
   {
-    "revision": "d96a15a4443a5e2b7f9c",
-    "url": "https://douban-1300076408.cos.ap-guangzhou.myqcloud.com/static/css/main.2df7413e.chunk.css"
+    "revision": "bd63ef3d34687249fd77",
+    "url": "https://introfei.github.io/douban/static/css/main.1ea74ac1.chunk.css"
   },
   {
     "revision": "0c762094c435d895f983",
-    "url": "https://douban-1300076408.cos.ap-guangzhou.myqcloud.com/static/js/2.71205f64.chunk.js"
+    "url": "https://introfei.github.io/douban/static/js/2.71205f64.chunk.js"
   },
   {
-    "revision": "d96a15a4443a5e2b7f9c",
-    "url": "https://douban-1300076408.cos.ap-guangzhou.myqcloud.com/static/js/main.30f5302f.chunk.js"
+    "revision": "bd63ef3d34687249fd77",
+    "url": "https://introfei.github.io/douban/static/js/main.bbe9476d.chunk.js"
   },
   {
     "revision": "8c97409f0ee389fe75da",
-    "url": "https://douban-1300076408.cos.ap-guangzhou.myqcloud.com/static/js/runtime~main.d653cc00.js"
+    "url": "https://introfei.github.io/douban/static/js/runtime~main.d653cc00.js"
   },
   {
     "revision": "45afa4d7db6d97eeb6f6393888e61b49",
-    "url": "https://douban-1300076408.cos.ap-guangzhou.myqcloud.com/static/media/SourceHanSansCN-ExtraLight.45afa4d7.otf"
+    "url": "https://introfei.github.io/douban/static/media/SourceHanSansCN-ExtraLight.45afa4d7.otf"
   },
   {
     "revision": "56ce42be8c3a9830d8634ccb9b22fe12",
-    "url": "https://douban-1300076408.cos.ap-guangzhou.myqcloud.com/static/media/icomoon.56ce42be.svg"
+    "url": "https://introfei.github.io/douban/static/media/icomoon.56ce42be.svg"
   },
   {
     "revision": "6190ed2439225e1e9ef954e9b537ac55",
-    "url": "https://douban-1300076408.cos.ap-guangzhou.myqcloud.com/static/media/icomoon.6190ed24.woff"
+    "url": "https://introfei.github.io/douban/static/media/icomoon.6190ed24.woff"
   },
   {
     "revision": "791e13e2920e3a198e35f5ea63e931e0",
-    "url": "https://douban-1300076408.cos.ap-guangzhou.myqcloud.com/static/media/icomoon.791e13e2.eot"
+    "url": "https://introfei.github.io/douban/static/media/icomoon.791e13e2.eot"
   },
   {
     "revision": "ca96d3e60ac933669941f640500e8f5a",
-    "url": "https://douban-1300076408.cos.ap-guangzhou.myqcloud.com/static/media/icomoon.ca96d3e6.ttf"
+    "url": "https://introfei.github.io/douban/static/media/icomoon.ca96d3e6.ttf"
   }
 ]);
